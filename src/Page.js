dad47e5ffdd2080b3ea758108020e5161fbcebb7
Hammad Khan <khanhammad896@gmail.com>
2021-11-30 13:06:21 +0500

    First commit

---
+++ src/Page.js
@@ -0,0 +1,141 @@
+import React from "react";
+import "./Page.css";
+import { Box, Button } from "@material-ui/core";
+import AddPage from "./components/AddPage";
+const Page = () => {
+  const [open, setOpen] = React.useState(false);
+  const [pages, setPages] = React.useState({
+    pages: [],
+  });
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const addPage = () => {
+    const newPage = JSON.parse(JSON.stringify(pages));
+    newPage.pages.push({
+      id: newPage.pages.length,
+      pageName: "",
+      pageDescription: "",
+      content: [],
+    });
+    setPages(newPage);
+  };
+
+  const setFields = (p_id, name, value) => {
+    const newPage = JSON.parse(JSON.stringify(pages));
+    const page = newPage.pages.find((p) => p.id === p_id);
+    page[name] = value;
+    setPages(newPage);
+  };
+
+  const setContentFields = (p_id, c_id, name, value) => {
+    const newPage = JSON.parse(JSON.stringify(pages));
+    const newContent = newPage.pages
+      .find((p) => p_id === p.id)
+      .content.find((c) => c_id === c.id);
+    newContent[name] = value;
+    setPages(newPage);
+  };
+
+  const setListFields = (p_id, c_id, l_id, value) => {
+    const newPage = JSON.parse(JSON.stringify(pages));
+    const newListItem = newPage.pages
+      .find((p) => p_id === p.id)
+      .content.find((c) => c.id === c_id)
+      .listValue.find((l) => l.id === l_id);
+    newListItem["value"] = value;
+    setPages(newPage);
+  };
+
+  const handleDelete = (p_id, type, c_id = null) => {
+    const newPage = JSON.parse(JSON.stringify(pages));
+    const newContent = newPage.pages.find((p) => p_id === p.id).content;
+    if (type === 0) {
+      newPage.pages.splice(
+        newPage.pages.findIndex((p) => p.id === p_id),
+        1
+      );
+    }
+    if (type === 1) {
+      newContent.splice(
+        newContent.findIndex((c) => c.id === c_id),
+        1
+      );
+    }
+    setPages(newPage);
+  };
+
+  const addContent = (p_id, type) => {
+    const newPage = JSON.parse(JSON.stringify(pages));
+    const newContent = newPage.pages.find((p) => p.id === p_id).content;
+    if (type === 0) {
+      newContent.push({
+        id: newContent.length,
+        textName: "",
+        textValue: "",
+        type: "Text",
+      });
+    }
+    if (type === 1) {
+      newContent.push({
+        id: newContent.length,
+        numberName: "",
+        numberValue: "",
+        type: "Number",
+      });
+    }
+    if (type === 2) {
+      newContent.push({
+        id: newContent.length,
+        listName: "",
+        listValue: [],
+        type: "List",
+      });
+    }
+    setPages(newPage);
+  };
+
+  const addList = (p_id, c_id) => {
+    const newPage = JSON.parse(JSON.stringify(pages));
+    const newList = newPage.pages
+      .find((p) => p.id === p_id)
+      .content.find((c) => c_id === c.id).listValue;
+    newList.push({
+      id: newList.length,
+      value: "",
+    });
+    setPages(newPage);
+  };
+
+  console.log("Pages >>> ", pages);
+  return (
+    <>
+      <Box>
+        <div className="btn-container">
+          <Button variant="contained" color="primary" onClick={handleClickOpen}>
+            Add Page
+          </Button>
+        </div>
+        <AddPage
+          open={open}
+          handleClose={handleClose}
+          addPage={addPage}
+          pages={pages.pages}
+          setFields={setFields}
+          handleDelete={handleDelete}
+          addContent={addContent}
+          setContentFields={setContentFields}
+          addList={addList}
+          setListFields={setListFields}
+        />
+      </Box>
+    </>
+  );
+};
+
+export default Page;
---
+++ src/components/AddPage.js
@@ -0,0 +1,263 @@
+import React from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Slide,
+  Button,
+  TextField,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import "./AddPage.css";
+import { IoAddCircleOutline } from "react-icons/io5";
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const AddPage = ({
+  open,
+  handleClose,
+  addPage,
+  pages,
+  setFields,
+  handleDelete,
+  addContent,
+  setContentFields,
+  addList,
+  setListFields,
+}) => {
+  const data = pages;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = (p_id, type) => {
+    addContent(p_id, type);
+    setAnchorEl(null);
+  };
+
+  const getNameByType = (value) => {
+    if (value === "Text") {
+      return {
+        name: "textName",
+        value: "textValue",
+      };
+    }
+    if (value === "Number") {
+      return {
+        name: "numberName",
+        value: "numberValue",
+      };
+    }
+    if (value === "List") {
+      return {
+        name: "listName",
+        value: "listValue",
+      };
+    }
+  };
+  return (
+    <>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">{"Add Page"}</DialogTitle>
+        <DialogContent>
+          {data.map((page) => (
+            <div>
+              <span
+                style={{
+                  fontSize: 14,
+                  color: "rgba(0, 0, 0, 0.7)",
+                }}
+              >
+                Page {`${data.indexOf(page) + 1}`}
+              </span>
+              <div className="page-container" key={page.id}>
+                <TextField
+                  variant="outlined"
+                  label="Page Name"
+                  fullWidth
+                  name="pageName"
+                  style={{ margin: "10px 0" }}
+                  onChange={(event) =>
+                    setFields(page.id, "pageName", event.target.value)
+                  }
+                  value={page.pageName}
+                />
+                <TextField
+                  variant="outlined"
+                  label="Page Description"
+                  fullWidth
+                  name="pageDescription"
+                  style={{ margin: "10px 0" }}
+                  onChange={(event) =>
+                    setFields(page.id, "pageDescription", event.target.value)
+                  }
+                  value={page.pageDescription}
+                />
+                <div
+                  className="bin-container"
+                  onClick={() => handleDelete(page.id, 0)}
+                >
+                  X
+                </div>
+                <span
+                  style={{
+                    fontSize: 14,
+                    color: "rgba(0, 0, 0, 0.5",
+                  }}
+                >
+                  Page Content{" "}
+                </span>
+                <div className="content-container">
+                  {page.content.map((content) => (
+                    <div>
+                      <span
+                        style={{
+                          fontSize: 14,
+                          color: "rgba(0, 0, 0, 0.5",
+                        }}
+                      >
+                        {content.type}{" "}
+                      </span>
+                      <div className="inputs-container" key={content.id}>
+                        <TextField
+                          variant="outlined"
+                          label="Name"
+                          fullWidth
+                          name={getNameByType(content.type).name}
+                          style={{ margin: "10px 0" }}
+                          onChange={(event) =>
+                            setContentFields(
+                              page.id,
+                              content.id,
+                              event.target.name,
+                              event.target.value
+                            )
+                          }
+                          value={content.textName}
+                        />
+                        {(content.type === "Text" ||
+                          content.type === "Number") && (
+                          <TextField
+                            variant="outlined"
+                            label="Value"
+                            fullWidth
+                            name={getNameByType(content.type).value}
+                            type={content.type === "Text" ? "text" : "number"}
+                            style={{ margin: "10px 0" }}
+                            onChange={(event) =>
+                              setContentFields(
+                                page.id,
+                                content.id,
+                                event.target.name,
+                                event.target.value
+                              )
+                            }
+                            value={content.textValue}
+                          />
+                        )}
+                        {content.type === "List" && (
+                          <>
+                            {content.listValue.map((list) => (
+                              <TextField
+                                variant="outlined"
+                                label={`Value #${
+                                  content.listValue.indexOf(list) + 1
+                                }`}
+                                fullWidth
+                                name={getNameByType(content.type).value}
+                                style={{ margin: "10px 0" }}
+                                onChange={(event) =>
+                                  setListFields(
+                                    page.id,
+                                    content.id,
+                                    list.id,
+                                    event.target.value
+                                  )
+                                }
+                                // value={
+                                //   content.listValue[
+                                //     content.listValue.indexOf(list)
+                                //   ].value
+                                // }
+                              />
+                            ))}
+                            <div
+                              className="add-btn-container"
+                              onClick={() => addList(page.id, content.id)}
+                            >
+                              <IoAddCircleOutline /> <span>Add items</span>
+                            </div>
+                          </>
+                        )}
+                        <div
+                          className="bin-container"
+                          onClick={() => handleDelete(page.id, 1, content.id)}
+                        >
+                          X
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                  <div className="inner-btn-container">
+                    <Button
+                      aria-controls="simple-menu"
+                      aria-haspopup="true"
+                      variant="outlined"
+                      color="primary"
+                      onClick={handleClick}
+                      // onClick={addPage}
+                    >
+                      Add Content
+                    </Button>
+                    <Menu
+                      id="simple-menu"
+                      anchorEl={anchorEl}
+                      keepMounted
+                      open={Boolean(anchorEl)}
+                      onClose={handleMenuClose}
+                    >
+                      <MenuItem onClick={() => handleMenuClose(page.id, 0)}>
+                        Text
+                      </MenuItem>
+                      <MenuItem onClick={() => handleMenuClose(page.id, 1)}>
+                        Number
+                      </MenuItem>
+                      <MenuItem onClick={() => handleMenuClose(page.id, 2)}>
+                        List
+                      </MenuItem>
+                      <MenuItem onClick={() => handleMenuClose(page.id, 3)}>
+                        Images
+                      </MenuItem>
+                    </Menu>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+          <Button variant="outlined" color="primary" onClick={addPage}>
+            Add Page
+          </Button>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" variant="contained">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default AddPage;
